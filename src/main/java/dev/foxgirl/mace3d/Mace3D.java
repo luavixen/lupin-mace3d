d84ebb28925e74d3e29d3e620de5891d7ae31c91
Lua MacDougall <lua@foxgirl.dev>
2024-06-14 17:37:26 -0700

    Migrate to mod-only with mixins for 2D icon

--- src/main/java/dev/foxgirl/mace3d/Mace3D.java
+++ src/main/java/dev/foxgirl/mace3d/Mace3D.java
@@ -1,36 +1,16 @@
 package dev.foxgirl.mace3d;
 
-import com.google.common.io.CharStreams;
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.util.Identifier;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.concurrent.Executors;
 
 public final class Mace3D implements ClientModInitializer {
 
+    public static final ModelIdentifier MACE = ModelIdentifier.ofInventoryVariant(Identifier.ofVanilla("mace"));
+    public static final ModelIdentifier MACE_IN_HAND = ModelIdentifier.ofInventoryVariant(Identifier.ofVanilla("mace_in_hand"));
+
     @Override
     public void onInitializeClient() {
-        Executors.newScheduledThreadPool(1).schedule(() -> {
-            MinecraftClient.getInstance().execute(() -> {
-                /*try {
-                    var resource = MinecraftClient.getInstance().getResourceManager().getResourceOrThrow(Identifier.of("mace3d", "icon.png"));
-                    System.out.println(resource);
-                    System.out.println(CharStreams.toString(new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8)));
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }*/
-                MinecraftClient.getInstance().getResourceManager().findAllResources("textures", __ -> true).forEach((id, resource) -> {
-                    if (id.toString().contains("mace3d")) {
-                        System.out.println(resource);
-                    }
-                });
-            });
-        }, 5, java.util.concurrent.TimeUnit.SECONDS);
     }
 
 }
